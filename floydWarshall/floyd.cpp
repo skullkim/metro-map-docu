83f1688cc30312765589e5737e04743ebbcf2adc
skullkim <space1878@naver.com>
2021-09-13 12:39:56 +0900

    추가: 최소 가중치로 연산시 경로 출력 기능 추가

--- floydWarshall/floyd.cpp
+++ floydWarshall/floyd.cpp
@@ -4,11 +4,24 @@
 #include <fstream>
 using namespace std;
 
+const int MAX = 1000;
+const int INF = 987654321;
+int path[MAX][MAX];
+vector<int> tmp;
+
+void findPath(int from, int to) {
+	int prev = path[from][to];
+	if(from == prev) {
+		tmp.push_back(from);
+		return;
+	}
+	findPath(from, prev);
+	findPath(prev, to);
+}
+
 int main(void) {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	const int MAX = 1000;
-	const int INF = 987654321;
 	int graph[MAX][MAX];
 	vector<int> stations;
 	for(int i = 0, s; i < 139; i++) {
@@ -23,6 +36,8 @@ int main(void) {
 			int s2 = stations[k];
 			graph[s1][s2] = INF;
 			graph[s2][s1] = INF;
+			path[s1][s2] = s1;
+			path[s2][s1] = s2;
 		}
 	}
 	for(int i = 0; i < 139; i++) {
@@ -36,11 +51,21 @@ int main(void) {
 				int st_s = stations[st];
 				int mid_s = stations[mid];
 				int e_s = stations[e];
-				graph[st_s][e_s] = min(graph[st_s][e_s], graph[st_s][mid_s] + graph[mid_s][e_s]); 
+				if(graph[st_s][e_s] > graph[st_s][mid_s] + graph[mid_s][e_s]) {
+					graph[st_s][e_s] = graph[st_s][mid_s] + graph[mid_s][e_s];
+					path[st_s][e_s] = mid_s;
+				}
 			}
 		}
 	}	
 	int from, to;
 	cin >> from >> to;
-	cout << graph[from][to];
+	cout << graph[from][to] << "\n";
+	tmp.clear();
+	findPath(from, to);
+	tmp.push_back(to);
+	for(auto p : tmp) {
+		cout << p << " ";
+	}
+	cout << "\n";
 }
